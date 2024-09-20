8674136ec05637439f26f5c45a6ea21e8abf00c1
SimpleTime <a1973368986@outlook.com>
2024-09-20 10:11:01 +0800

    Update 3change.C

--- C/3change.C
+++ C/3change.C
@@ -1,8 +1,17 @@
 #include <stdio.h>
+const int MONEY = 100;
 int main(){
+
+    /*
     int price;
     printf("请输入金额:");
     scanf("%d",&price);
-    printf("找回%d元\n",100-price);
+    printf("找回%d元\n",MONEY-price);
     return 0; 
+    */
+
+    int a,b;
+    printf("输入：");
+    scanf("%d %d",&a,&b);
+    printf("%d + %d = %d",a,b,a+b);
 }
