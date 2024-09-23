5fc6335bbe04f6d7911cbf08bd25dae513a40ffc
SimpleTime <a1973368986@outlook.com>
2024-09-23 22:03:23 +0800

    12

--- C++/Lan.cpp
+++ C++/Lan.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
+    long long a,b,x,re;
+    cin>>a>>b>>x;
+    re = a;
+    for (size_t c = 0; c < b-1; c++){
+        re = re*a;
+    }
+    //re = pow(a,b);
+    cout<< re <<endl;
+    string str = to_string(re);
+    string ans;
+    for (size_t i = x; i > 0 ; i--){
+        ans.append({str[str.size()-i]});
+        //cout<<ans<<endl;
+    }
+    cout<<ans;
     return 0;
 }
