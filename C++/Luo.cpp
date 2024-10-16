0532300f22c9d69a7c1914b0a3f7e397686437b4
SimpleTime <a1973368986@outlook.com>
2024-10-16 18:30:08 +0800

    6

--- C++/Luo.cpp
+++ C++/Luo.cpp
@@ -1,8 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    char a = '9';
-	char b = 9;
-	cout<<a<<endl;
-	cout<<b;
+	int n=0;
+	float s,l=0;
+	cin>>s;
+	for (float i = 2; l <= s; i = i*0.98){
+		l += i;
+		n++;
+	}
+	cout<<n;
 }
